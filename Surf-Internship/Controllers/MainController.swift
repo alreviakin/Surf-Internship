cbaacd14b7cf732bb099edcccdadf8e5125ba296
aleksejrevakin <alexey.amahasla@gmail.com>
2023-03-01 15:38:54 +0300

    Create alert, senderButton and quetsioinsLabel

--- Surf-Internship/Controllers/MainController.swift
+++ Surf-Internship/Controllers/MainController.swift
@@ -11,36 +11,36 @@ import SnapKit
 class MainController: UIViewController {
     
     //MARK: - Variables
-    private let enterButton: Int?
+    private var enterButton = -1
     private let scrollView: UIScrollView = {
-       let scroll = UIScrollView()
+        let scroll = UIScrollView()
         scroll.isScrollEnabled = true
-        scroll.contentInsetAdjustmentBehavior = .never
         scroll.showsVerticalScrollIndicator = false
         scroll.bounces = false
         return scroll
     }()
     private let backgroundImageView: UIImageView = {
-       let imageView = UIImageView()
+        let imageView = UIImageView()
         imageView.image = Resources.Image.backgroundImage
         imageView.contentMode = .scaleToFill
         return imageView
     }()
     private let backgroundView: UIView = {
-       let view = UIView()
+        let view = UIView()
         view.layer.cornerRadius = 32
         view.clipsToBounds = true
         view.backgroundColor = .white
         return view
     }()
     private let nameLabel: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.text = "Стажировка в Surf"
         label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .black
         return label
     }()
     private let descriptionLabel: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.text = "Работай над реальными задачами под руководством опытного наставника и получи возможность стать частью команды мечты."
         label.font = UIFont.systemFont(ofSize: 13, weight: .light)
         label.textColor = UIColor(hexString: "#96959B")
@@ -48,43 +48,77 @@ class MainController: UIViewController {
         return label
     }()
     private let horiontalScroll: UIScrollView = {
-       let scroll = UIScrollView()
+        let scroll = UIScrollView()
         scroll.showsHorizontalScrollIndicator = false
         scroll.isScrollEnabled = true
         return scroll
     }()
     private let iosButton: UIButton = {
         let button = UIButton().createMyButton(name: "IOS")
-       return button
+        return button
     }()
     private let androidButton: UIButton = {
         let button = UIButton().createMyButton(name: "Android")
-       return button
+        return button
     }()
     private let designButton: UIButton = {
         let button = UIButton().createMyButton(name: "Design")
-       return button
+        return button
     }()
     private let flutterButton: UIButton = {
         let button = UIButton().createMyButton(name: "Flutter")
-       return button
+        return button
     }()
     private let qaButton: UIButton = {
         let button = UIButton().createMyButton(name: "QA")
-       return button
+        return button
     }()
     private let pmButton: UIButton = {
         let button = UIButton().createMyButton(name: "PM")
-       return button
+        return button
     }()
-    private lazy var horizontalButtons: [UIButton] = [iosButton, androidButton, designButton, flutterButton, qaButton, pmButton]
+    private let pythonButton: UIButton = {
+        let button = UIButton().createMyButton(name: "Python")
+        return button
+    }()
+    private let javaButton: UIButton = {
+        let button = UIButton().createMyButton(name: "Java")
+        return button
+    }()
+    private let kotlinButton: UIButton = {
+        let button = UIButton().createMyButton(name: "Kotlin")
+        return button
+    }()
+    private let cButton: UIButton = {
+        let button = UIButton().createMyButton(name: "C++")
+        return button
+    }()
+    private lazy var horizontalButtons: [UIButton] = [iosButton, androidButton, designButton, flutterButton, qaButton, pmButton, pythonButton, javaButton, kotlinButton, cButton]
     private let suggestionLabel: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.text = "Получай стипендию, выстраивай удобный график, работай на современном железе."
         label.font = UIFont.systemFont(ofSize: 13, weight: .light)
         label.textColor = UIColor(hexString: "#96959B")
         label.numberOfLines = 0
         return label
+    }()
+    private let questionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Хочешь к нам?"
+        label.font = UIFont.systemFont(ofSize: 13, weight: .light)
+        label.textColor = UIColor(hexString: "#96959B")
+        return label
+    }()
+    private lazy var sendButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Отправить заявку", for: .normal)
+        button.backgroundColor = .black
+        button.layer.cornerRadius = 30
+        button.clipsToBounds = true
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        button.addTarget(self, action: #selector(send), for: .touchUpInside)
+        return button
     }()
     
     override func viewDidLoad() {
@@ -96,36 +130,41 @@ class MainController: UIViewController {
     
     private func configuration() {
         view.backgroundColor = .white
-        scrollView.contentSize = CGSize(width: view.bounds.width, height: view.bounds.height * 1.6)
+        scrollView.contentSize = CGSize(width: view.bounds.width, height: view.bounds.height * 1.45)
+        view.addSubview(backgroundImageView)
         view.addSubview(scrollView)
-        scrollView.addSubview(backgroundImageView)
         scrollView.addSubview(backgroundView)
         backgroundView.addSubview(nameLabel)
         backgroundView.addSubview(descriptionLabel)
-        horiontalScroll.contentSize = CGSize(width: view.bounds.width * 2, height: 44)
         backgroundView.addSubview(horiontalScroll)
+        var countSymbol = 0
         for index in 0..<horizontalButtons.count {
             let button = horizontalButtons[index]
             horiontalScroll.addSubview(button)
             button.addTarget(self, action: #selector(enterButton(sender:)), for: .touchUpInside)
             button.tag = index
+            countSymbol += (button.titleLabel?.text?.count)!
         }
+        let widthContentSize = countSymbol * 8 + horizontalButtons.count * 48 + (horizontalButtons.count - 1) * 12 + 10
+        horiontalScroll.contentSize = CGSize(width: widthContentSize, height: 44)
         backgroundView.addSubview(suggestionLabel)
+        view.addSubview(questionLabel)
+        view.addSubview(sendButton)
     }
     
     private func layout() {
         scrollView.snp.makeConstraints { make in
             make.left.right.bottom.equalToSuperview()
-            make.top.equalToSuperview()
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
         }
         
         backgroundImageView.snp.makeConstraints { make in
-            make.width.equalTo(scrollView)
+            make.width.equalTo(view)
             make.height.equalTo(view.bounds.height * 0.85)
             make.top.equalToSuperview()
         }
         backgroundView.snp.makeConstraints { make in
-            make.top.equalTo(backgroundImageView.snp.bottom).offset(-view.bounds.height * 0.15)
+            make.top.equalToSuperview().offset(view.bounds.height * 0.55)
             make.width.left.equalToSuperview()
             make.height.equalTo(view.bounds.height * 0.9)
         }
@@ -144,42 +183,85 @@ class MainController: UIViewController {
             make.right.equalToSuperview()
             make.height.equalTo(44)
         }
-        for index in 0..<horizontalButtons.count {
-            let button = horizontalButtons[index]
-            button.titleLabel?.snp.makeConstraints({ make in
-                make.left.equalToSuperview().offset(24)
-                make.right.equalToSuperview().offset(-24)
-            })
-            if index == 0 {
-                button.snp.makeConstraints { make in
-                    make.left.top.equalToSuperview()
-                    make.height.equalTo(44)
-                }
-                continue
-            }
-            let beforeButton = horizontalButtons[index-1]
-            button.snp.makeConstraints { make in
-                make.left.equalTo(beforeButton.snp.right).offset(12)
-                make.top.equalToSuperview()
-                make.height.equalTo(44)
-            }
-        }
+        layoutButtons()
         suggestionLabel.snp.makeConstraints { make in
             make.top.equalTo(horiontalScroll.snp.bottom).offset(24)
             make.left.equalTo(nameLabel)
             make.right.equalToSuperview().offset(-20)
         }
+        questionLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(20)
+            make.bottom.equalToSuperview().offset(-78)
+        }
+        sendButton.snp.makeConstraints { make in
+            make.height.equalTo(60)
+            make.left.equalTo(questionLabel.snp.right).offset(24)
+            make.centerY.equalTo(questionLabel)
+        }
+        sendButton.titleLabel?.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(44)
+            make.right.equalToSuperview().offset(-44)
+        }
+    }
+    
+    private func layoutButtons() {
+        for index in 0..<horizontalButtons.count {
+            let button = horizontalButtons[index]
+            button.titleLabel?.snp.remakeConstraints({ make in
+                make.left.equalToSuperview().offset(24)
+                make.right.equalToSuperview().offset(-24)
+            })
+            if index == 0 {
+                button.snp.remakeConstraints { make in
+                    make.left.top.equalToSuperview()
+                    make.height.equalTo(43)
+                }
+                continue
+            }
+            let beforeButton = horizontalButtons[index-1]
+            button.snp.remakeConstraints { make in
+                make.left.equalTo(beforeButton.snp.right).offset(12)
+                make.top.equalToSuperview()
+                make.height.equalTo(43)
+            }
+        }
     }
 }
 
-extension MainController {
-    @objc private func enterButton(sender: UIButton) {
-        for button in horizontalButtons {
-            button.backgroundColor = UIColor(hexString: "#F3F3F5")
-            button.setTitleColor(.black, for: .normal)
+@objc extension MainController {
+    private func enterButton(sender: UIButton) {
+        UIView.animate(withDuration: 0.5) { [self] in
+            for index in 0..<self.horizontalButtons.count {
+                let button = self.horizontalButtons[index]
+                button.backgroundColor = UIColor(hexString: "#F3F3F5")
+                button.setTitleColor(.black, for: .normal)
+                button.tag = index
+            }
+            
+            
+            if sender.tag != self.enterButton{
+                sender.backgroundColor = .black
+                sender.setTitleColor(.white, for: .normal)
+                self.enterButton = sender.tag
+            }else {
+                self.enterButton = -1
+            }
         }
-        sender.backgroundColor = .black
-        sender.setTitleColor(.white, for: .normal)
-        enterButton = sender.tag
     }
+    
+    private func send() {
+        guard enterButton >= 0 else {
+            let alert = UIAlertController(title: "Выберете пожалуйста направление", message: nil, preferredStyle: .alert)
+            let action = UIAlertAction(title: "Закрыть", style: .cancel)
+            alert.addAction(action)
+            present(alert, animated: true)
+            return
+        }
+        let direction = (horizontalButtons[enterButton].titleLabel?.text)!
+        let alert = UIAlertController(title: "Поздравляем!", message: "Ваша заявка успешно отправлена! Направление \(direction)", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Закрыть", style: .cancel)
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
 }
